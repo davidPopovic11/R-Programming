642246f8111be0fd00c06ff2151e3ef5f2529abd
davidPopovic11 <dpopovicw@gmail.com>
2025-05-11 00:48:48 +0200

    Needs editing, code works and all tasks are correct

---
+++ quiz1.R
@@ -0,0 +1,24 @@
+dataRead <- read.csv("hw1_data.csv")
+i <- dataRead[,1]
+j <- dataRead[,2, drop = FALSE]
+k <- dataRead[,4, drop = FALSE]
+
+good <- i[!is.na(i)]
+bad <- i[is.na(i)]
+
+
+idf <- dataRead[,1, drop = FALSE]
+binded <- cbind(idf,j, k)
+#cleanidf <- idf[complete.cases(idf), , drop = FALSE]
+#cleanj <- j[complete.cases(j), , drop = FALSE]
+
+
+subs <- binded[binded$Ozone>31 & binded$Temp>90, ,drop = FALSE]
+
+t <-subs[, 2]
+goodt <- t[!is.na(t)]
+
+l1 <- dataRead[,4:5, drop = FALSE]
+l <- l1[l1$Month == 6, , drop = FALSE]
+
+l11 <- l[,1]
